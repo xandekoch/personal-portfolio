9bc577cdd1253875afaa01af67bf5b35bda3e221
Alexandre Koch <85593242+xandekoch@users.noreply.github.com>
2022-10-18 01:34:42 -0300

    dark theme added, skills section added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,27 @@
 /*=============== TOGGLE TABS ===============*/
+function activeProjects() {
+    if ('projectsActive' !== alternateSections.classList[1]) {
+        alternateSections.classList.add("projectsActive");
+        alternateSections.classList.remove("skillsActive");
+    }
+}
 
+function activeSkills() {
+    if ('skillsActive' !== alternateSections.classList[1]) {
+        alternateSections.classList.add("skillsActive");
+        alternateSections.classList.remove("projectsActive");
+    }
+}
 
 /*=============== DARK LIGHT THEME ===============*/
+function changeTheme() {
+    if ('dark' == theme.classList[0]) {
+        theme.classList.remove("dark");
+    }
+    else {
+        theme.classList.add("dark")
+    }
+}
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
